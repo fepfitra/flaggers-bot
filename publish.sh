b4757330d370365484a93d80b87c843a27db550b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-03-10 16:29:01 +0700

    cicd: use faster update cargo.lock

--- publish.sh
+++ publish.sh
@@ -47,8 +47,7 @@ echo "New version: $NEW_VERSION"
 
 sed -i "s/^version = \".*\"/version = \"$NEW_VERSION\"/" "$CARGO_FILE"
 
-# update Cargo.lock
-cargo build --release
+cargo update
 
 git add "$CARGO_FILE" Cargo.lock
 git commit -m "chore: bump version to v$NEW_VERSION"
